5882520072fc2860b3914d827e40ac0c23fa0fa7
Pedro Silva <pedrosadultos@hotmail.com>
2026-05-07 02:55:42 -0300

    pagina de simulações e inicio do formulario fid + dmate

---
+++ frontend/components/forms/Fide.tsx
@@ -0,0 +1,17 @@
+interface FideProps {
+  fideData: any;
+  setFideData: (data: any) => void;
+}
+
+export default function Fide({ fideData, setFideData }: FideProps) {
+    return (
+        <div className="space-y-4">
+            
+            {/* 1. IDENTIFICAÇÃO */}
+            <div className="flex flex-col">
+
+            </div>
+
+        </div>
+    )
+}
